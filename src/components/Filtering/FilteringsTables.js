dc1adfeaaa966d54084faab8b75947de3675e88f
Jelson J <jelson20@outlook.com>
2021-02-22 16:46:06 +0000

    commit pagination page

--- src/components/Filtering/FilteringsTables.js
+++ src/components/Filtering/FilteringsTables.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import  {useAsyncDebounce} from 'react-table'
 
 export const Filterings = ({filter, setFilter}) => {
-//const { filterValue, setFilter} = items
+const { value, setValue} = useState(filter)
+
+const onChange = useAsyncDebounce((value ) => {
+  setFilter(value || undefined)
+}, 1000) 
+
   return (
     <div className='search'>
     Search:
-    <input name='name' value={filter || ''} onChange={(e) => setFilter(e.target.value)} />
+    <input name='name' value={value || ''} 
+    onChange={(e) => 
+      {setValue(e.target.value) 
+      onChange(e.target.value)}} />
     </div>
   )
 }
--- src/components/Table/Header.js
+++ src/components/Table/Header.js
@@ -4,7 +4,6 @@ import { ColumnFilter } from '../Column/ColumnFilter'
 export const HeaderColumns = [
   {Header: 'ID', Footer:'ID', 
   accessor: 'id',
-  Filter: ColumnFilter,
   disableFilters: true
   
   },
@@ -12,37 +11,36 @@ export const HeaderColumns = [
   {Header: 'First Name', 
   Footer:'First Name', 
   accessor: 'first_name',
-  Filter: ColumnFilter
+  
  },
   {Header: 'Last Name', 
   Footer: 'Last Name', 
   accessor:'last_name',
-  Filter: ColumnFilter
+  
  },
   {Header: 'Country', 
   Footer:'Country', 
   accessor:'country',
-  Filter: ColumnFilter
+  
 },
   {Header: 'Telephone', 
   Footer: 'Telephone', 
   accessor: 'telephone',
-  Filter: ColumnFilter
+  
 },
   {Header: 'Date of Birth', 
   Footer: 'Date of Birth', 
   accessor: 'date_of_birth',
    Cell: ({value}) => {return format(new Date(value), 'dd/MM/yyy')},
-   Filter: ColumnFilter
+  
   },
   {Header: 'Age', 
   Footer: 'Age', 
   accessor: 'age',
-  Filter: ColumnFilter
+
  },
   {Header: 'Email', 
   Footer: 'Email', 
-  accessor: 'email',
-  Filter: ColumnFilter
+  accessor: 'email'
 }
 ]
